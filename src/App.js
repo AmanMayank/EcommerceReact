55ff580c03ab07f51a308f133f71ce3c3890f727
Aman Mayank <aman.msingh87@gmail.com>
2020-11-26 05:20:11 +0530

    Create Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homescreen from './screens/Homescreen'
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Ecommerce</h1>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Homescreen/>
+        </Container>
+      </main>
+      <Footer/>
     </div>
   );
 }
